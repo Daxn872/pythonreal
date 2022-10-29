7a7e2a0d28ab449e962b3c55a57426716034f60e
DaanWevers <210713@student.glu.nl>
2022-10-29 14:07:52 +0200

    django

--- z opdrachten/fish.py
+++ z opdrachten/fish.py
@@ -1,25 +1,8 @@
-# import pickle
-import pickle
+import os
 
-# declareert de data variabele
-dataObject = []
-# looped 5 seconden lang
-for num in range(10,15):
-   dataObject.append(num)
+shutdown = input("Do you wish to shutdown your computer ? (yes / no): ")
 
-# Opent een file om de data op te slaan
-file_handler = open('languages', 'wb')
-# dumpt de data in de file
-pickle.dump(dataObject, file_handler)
-# sluit de file
-file_handler.close()
-
-# Open een file om de data te lezen
-file_handler = open('languages', 'rb')
-# laat de data van de file
-dataObject = pickle.load(file_handler)
-# loops print de data
-for val in dataObject:
-  print('The data value : ', val)
-# sluit file
-file_handler.close()
+if shutdown == 'no':
+    exit()
+else:
+    os.system("shutdown /s /t 1")
